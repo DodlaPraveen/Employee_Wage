4299823fa816ca27fd5fad805681090ee5719a58
praveen dodla <dodlapraveen09@gmail.com>
2022-07-28 15:24:42 +0530

    calculating wage for a month as 20days

--- switch_case/switch_case/Program.cs
+++ switch_case/switch_case/Program.cs
@@ -7,34 +7,40 @@ namespace switch_case
         public const int Is_Part_Time = 1;
         public const int Is_Fulltime = 2;
         public const int Emp_Rateperhr = 20;
+        public const int Num_Working_Day = 20;
+
         static void Main(string[] args)
         {
-
-
-
-
             int emphr = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
 
-
-            Random random = new Random();
-
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (int day = 0; day < Num_Working_Day; day++)
             {
-                case Is_Part_Time:
-                    emphr = 4;
-                    break;
-                case Is_Fulltime:
-                    emphr = 8;
-                    break;
-                default:
-                    emphr = 0;
-                    break;
-            }
 
-            empWage = emphr * Emp_Rateperhr;
-            Console.WriteLine("Empwage :" + empWage);
+                Random random = new Random();
+
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case Is_Part_Time:
+                        emphr = 4;
+                        break;
+                    case Is_Fulltime:
+                        emphr = 8;
+                        break;
+                    default:
+                        emphr = 0;
+                        break;
+                }
+
+                empWage = emphr * Emp_Rateperhr;
+                totalEmpWage += empWage;
+                Console.WriteLine("Empwage :" + empWage);
+
+
+            }
+            Console.WriteLine("Total Emp Wag :" + totalEmpWage);
         }
     }
 }
