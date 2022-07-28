7d1d1998ccd1e242fe463e5dc79def2c1a3341f4
praveen dodla <dodlapraveen09@gmail.com>
2022-07-28 13:15:13 +0530

    switch case

---
+++ switch_case/switch_case/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        public const int Is_Part_Time = 1;
+        public const int Is_Fulltime = 2;
+        public const int Emp_Rateperhr = 20;
+        static void Main(string[] args)
+        {
+
+
+
+
+            int emphr = 0;
+            int empWage = 0;
+
+
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case Is_Part_Time:
+                    emphr = 4;
+                    break;
+                case Is_Fulltime:
+                    emphr = 8;
+                    break;
+                default:
+                    emphr = 0;
+                    break;
+            }
+
+            empWage = emphr * Emp_Rateperhr;
+            Console.WriteLine("Empwage :" + empWage);
+        }
+    }
+}
