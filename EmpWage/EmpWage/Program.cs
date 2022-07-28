39ba82b58f118601986664aa67c6ac9a45075ded
praveen dodla <dodlapraveen09@gmail.com>
2022-07-28 09:40:49 +0530

    calculating emp wage

---
+++ EmpWage/EmpWage/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Is_Fulltime = 1;
+            int Emp_Rateperhr = 20;
+
+            int emphr = 0;
+            int empWage = 0;
+
+
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == Is_Fulltime)
+            {
+                emphr = 8;
+            }
+            else
+            {
+                emphr = 0; 
+            }
+            empWage = emphr * Emp_Rateperhr;
+            Console.WriteLine("Empwage :"+empWage);
+        }
+    }
+}
