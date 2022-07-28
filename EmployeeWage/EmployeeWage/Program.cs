1ba84483f257e111fdc8852c7b2714eb31091174
praveen dodla <dodlapraveen09@gmail.com>
2022-07-28 09:10:06 +0530

    initiate uc_1

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Is_Emp = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Is_Emp)
+            {
+                Console.WriteLine("Emp is present");
+            }
+            else
+            {
+                Console.WriteLine("Emp is Absent");
+            }
+        }
+    }
+}
