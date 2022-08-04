b2d00d0de047bb79c8d08dc14c9d59c46045e5fd
praveen dodla <dodlapraveen09@gmail.com>
2022-08-04 14:34:44 +0530

    using interface approch

---
+++ switch_case/switch_case/IComputeEmpWage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWage
+{
+    interface IComputeEmpWage
+    {
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+        public void computeEmpWage();
+    }
+}
